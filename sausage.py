0a178b9c3b70a7f029e592811b9962083a147e7a
screaming in agony <ameliar51@nycstudents.net>
2026-03-02 09:54:55 -0500

    joys

--- sausage.py
+++ sausage.py
@@ -1,4 +1,4 @@
-def occupied(t,y,z):
+""" def occupied(t,y,z):
     x = 0
     for i in range(t):
         if y[i] == "c" and z[i] == "c":
@@ -6,4 +6,19 @@ def occupied(t,y,z):
     print(x)
     
         
-occupied(5,"c..c..",".c.cc")
+occupied(5,"c..c..",".c.cc") """
+
+def lang(a):
+    s = 0
+    t = 0
+    for i in a:
+        if i == "s" or i == "S":
+            s = s + 1
+        elif i == "t" or i == "T":
+            t = t + 1
+    if s >= t:
+        print("French") 
+    else:
+        print("English")
+
+lang("ttts")
