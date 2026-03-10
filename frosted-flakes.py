461e67a543419ba2ee71f70aabaff73ab70c4758
screaming in agony <ameliar51@nycstudents.net>
2026-03-10 10:27:16 -0400

    evil

--- frosted-flakes.py
+++ frosted-flakes.py
@@ -5,6 +5,7 @@ types = open("./types.json", encoding="utf8")
 t_data = json.load(types)
 t_datas = []
 lang_list = ["english, japanese, chinese, french"]
+
 """ lang = input("Choose a language: English, Japanese, Chinese, French ")
 x = lang.lower()
 
@@ -48,9 +49,20 @@ for item in data:
 for item in t_data:
     t_datas.append(item[x])
 typ = input(f"Select a type: {t_datas} ")
-y = typ.lower()
+y = typ.capitalize()
 
 for item in data:
-    for it in item["type"]:
-        if it == y:
+    for m in item["type"]:
+        if m == y:
             print(item["name"][x])
+
+search = input("Search for a pokemon:  ")
+for pokemon in data:
+    count = 0
+    s = 0
+    for char in pokemon["name"]:
+        if char == search[count]:
+            count += 1
+            s += 1
+    if s > len(search):
+        print(pokemon["name"][x])
